f17b747637c6d260f677719ca69a171a1f7b59bd
root <evchen>
2016-11-26 13:49:52 +0100

    adding makefile

--- dolly/dolly.c
+++ dolly/dolly.c
@@ -2,7 +2,19 @@
 #include <unistd.h>
 
 int main(){
+	int x = 42;	
 	int pid = fork();
-	printf("pid = %d\n",pid);
+	if (pid == 0)
+ 	{
+		x = 12;	
+		printf(" the process id of child is %d\n",getpid());
+		printf(" Hello, I'm the children and x is %d\n",x);
+	}
+		else{
+		printf(" the process id of mother is %d\n",getpid());	
+		x = 13;	
+		printf(" Hello, I'm the mother and x is %d\n",x);
+	}
+	//printf(" That's it %d\n", getpid());	
 	return 0;
 }
